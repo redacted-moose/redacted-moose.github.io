c202d35f9e1d528177040e0033aaf3be6f7595aa
Chris Sadler <sadlercr@rose-hulman.edu>
2016-06-02 11:41:29 +0200

    Class 11

---
+++ cube8.js
@@ -0,0 +1,204 @@
+
+
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var pointsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
+
+var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5, -0.5, -0.5, 1.0 ),
+    ];
+
+var vertexColors = [
+        vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+        vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+    ];
+
+var texCoord = [
+		vec2(0,0),
+		vec2(0,1),
+		vec2(1,1),
+		vec2(1,0)
+];
+
+var near = -1;
+var far = 1;
+var radius = 1.0;
+var theta  = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+var left = -1.0;
+var right = 1.0;
+var ytop = 1.0;
+var bottom = -1.0;
+
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var eye;
+
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+var texSize = 64;
+var numRows = 8;
+var numCols = 8;
+var myTexels = new Uint8Array(4*texSize*texSize);
+
+// quad uses first index to set color for face
+
+function quad(a, b, c, d) {
+     pointsArray.push(vertices[a]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[0]);
+	 
+     pointsArray.push(vertices[b]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[1]);
+	 
+     pointsArray.push(vertices[c]); 
+     colorsArray.push(vertexColors[a]);    
+	 texCoordsArray.push(texCoord[2]);
+	 
+     pointsArray.push(vertices[a]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[0]);
+
+     pointsArray.push(vertices[c]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[2]);
+
+     pointsArray.push(vertices[d]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[3]);
+}
+
+// Each face determines two triangles
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+
+window.onload = function init() {
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+	// Set up texture map
+	
+	for (var i=0; i<texSize; i++) {
+	   for (var j=0; j<texSize; j++) {
+	      var patchx = Math.floor(i/(texSize/numRows));
+		  var patchy = Math.floor(j/(texSize/numCols));
+		  var c = (patchx%2 !== patchy%2 ? 255 : 0);
+		  var loc = 4*i*texSize+4*j;
+		  myTexels[loc] = c;
+		  myTexels[loc+1] = c;
+		  myTexels[loc+2] = c;
+		  myTexels[loc+3] = 255;
+		}
+	}
+	
+	var texture = gl.createTexture();
+	gl.bindTexture (gl.TEXTURE_2D, texture);
+	gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, myTexels);
+    gl.generateMipmap( gl.TEXTURE_2D );
+	gl.uniform1i(gl.getUniformLocation(program, "texMap"), 0);
+		  
+    colorCube();
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+    
+    var vColor = gl.getAttribLocation( program, "vColor" );
+	gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+	
+	var tBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+	
+	var vTexCoord = gl.getAttribLocation (program, "vTexCoord");
+	gl.vertexAttribPointer (vTexCoord, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vTexCoord);
+ 
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+// buttons to change viewing parameters
+
+    document.getElementById("Button1").onclick = function(){near  *= 1.1; far *= 1.1;};
+    document.getElementById("Button2").onclick = function(){near *= 0.9; far *= 0.9;};
+    document.getElementById("Button3").onclick = function(){radius *= 1.1;};
+    document.getElementById("Button4").onclick = function(){radius *= 0.9;};
+    document.getElementById("Button5").onclick = function(){theta += dr;};
+    document.getElementById("Button6").onclick = function(){theta -= dr;};
+    document.getElementById("Button7").onclick = function(){phi += dr;};
+    document.getElementById("Button8").onclick = function(){phi -= dr;};
+       
+    render();
+}
+
+
+var render = function() {
+        gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            
+		eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
+			 radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+
+        modelViewMatrix = lookAt(eye, at , up); 
+        projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+        
+        gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+        gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+            
+        gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+        requestAnimFrame(render);
+    }
