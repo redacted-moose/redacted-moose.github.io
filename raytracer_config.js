2878dbf2c4c295e2c9fc42662e5408a1296fff8d
Chris Sadler <sadlercr@rose-hulman.edu>
2016-06-12 18:53:40 +0200

    Start work on raytracer

---
+++ raytracer_config.js
@@ -0,0 +1,14 @@
+var config = {
+	camera: {
+		eye: vec3(5.0, 5.0, 5.0),
+		at: vec3(0.0, 0.0, 0.0),
+		up: vec3(0.0, 1.0, 0.0)
+	},
+	plane: {
+		distance: 1,
+		fov: 45,
+		aspect: 1
+	},
+	objects: [
+	]
+};
