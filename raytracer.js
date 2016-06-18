510e1537db7dc83426bc241e06349a7959f775ec
Chris Sadler <sadlercr@rose-hulman.edu>
2016-06-18 23:29:17 +0200

    Start on lighting code

--- raytracer.js
+++ raytracer.js
@@ -1,94 +1,79 @@
 var canvas;
 var ctx;
 var canvasData;
+var config;
 
 var n, u, v;
 
 var height, width, center, topLeft;
 
-var backgroundColor = vec4(0, 0, 0, 255);
-
-function Entity (position, rotation, scale) {
-	this.position = position;
-	this.rotation = rotation;
-	this.scale = scale;
-}
-
-function Sphere (center, radius) {
-	Entity.call(this, center, vec3(0, 0, 0), vec3(radius, radius, radius));
-	// this.shader = new SphereShader("vertex-shader", "fragment-shader");
-	this.color = vec4(0, 85, 127, 255);
-}
-Sphere.prototype = Object.create(Entity.prototype);
-Sphere.prototype.intersect = function(ray) {
-	var d = subtract(ray.end, ray.start);
-	var a = dot(d, d);
-	var b = dot(scale(2, d), subtract(ray.start, this.position));
-	var c = dot(subtract(ray.start, this.position), subtract(ray.start, this.position)) - this.scale[0] * this.scale[0];
-	var discriminant = Math.sqrt(b*b - 4*a*c);
-
-	if (discriminant > 0) {
-		return (-b - discriminant)/(2*a);
-	} else {
-		return false;
-	}
-};
-
-function Ray (start, end) {
-	this.start = start;
-	this.end = end;
+function Ray(start, end) {
+    this.start = start || vec3();
+    this.end = end || vec3();
 }
 
 window.onload = function init() {
-	canvas = document.getElementById("canvas");
-	ctx = canvas.getContext('2d');
-	canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	n = normalize(subtract(config.camera.eye, config.camera.at));
-	// console.log(n);
-	u = normalize(cross(config.camera.up, n));
-	// console.log(u);
-	v = cross(n, u);
-	// console.log(v);
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext('2d');
+    canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    n = normalize(subtract(config.camera.eye, config.camera.at));
+    u = normalize(cross(config.camera.up, n));
+    v = cross(n, u);
 
-	height = 2*config.plane.distance * Math.tan(config.plane.fov/2);
-	width = height * config.plane.aspect;
-	center = subtract(config.camera.eye, scale(config.plane.distance, n));
-	// center - width/2 * u + height/2 * v
-	topLeft = add(subtract(center, scale(width/2, u)), scale(height/2, v));
-	render();
+    height = 2 * config.plane.distance * Math.tan(radians(config.plane.fov) / 2);
+    width = height * config.plane.aspect;
+    // eye - distance*n
+    center = subtract(config.camera.eye, scale(config.plane.distance, n));
+	// console.log(center);
+    // center - width/2 * u + height/2 * v
+    topLeft = add(subtract(center, scale(width / 2, u)), scale(height / 2, v));
+    render();
 };
 
 function render() {
-	var data = canvasData.data;
+	var result;
+    var t;
+    var object;
+    var data = canvasData.data;
+    data.setColor = function(x, y, color) {
+        var index = 4 * (canvas.width * y + x);
+        this[index] = color.r;
+        this[index + 1] = color.g;
+        this[index + 2] = color.b;
+        this[index + 3] = color.a;
+    };
 
-	var sphere = new Sphere(vec3(-3, 1, 2), 1);
+    var currRay = new Ray(config.camera.eye);
 
-	for (var y = 0; y < canvas.height; y++) {
-		for (var x = 0; x < canvas.width; x++) {
-			var t = Infinity;
-			// topLeft + x*width/canvas.width - y*height/canvas.height
-			var currRay = new Ray(config.camera.eye, add(topLeft,
-														 add(scale(x*width/canvas.width, u),
-															 scale(-y*height/canvas.height, v))));
-			var result = sphere.intersect(currRay);
-			if (result) {
-				t = result;
-			}
+    var intersect = function(currObject) {
+        var thing = currObject.intersect(currRay);
+        if (thing) {
+			result = thing;
+            t = thing.t;
+            object = currObject;
+        }
+    };
 
-			var index = 4 * (canvas.width * y + x);
-			if (t == Infinity) {
-				data[index] = backgroundColor[0];
-				data[index + 1] = backgroundColor[1];
-				data[index + 2] = backgroundColor[2];
-				data[index + 3] = backgroundColor[3];
-			} else {
-				data[index] = sphere.color[0];
-				data[index + 1] = sphere.color[1];
-				data[index + 2] = sphere.color[2];
-				data[index + 3] = sphere.color[3];
-			}
-		}
-	}
+    for (var y = 0; y < canvas.height; y++) {
+        for (var x = 0; x < canvas.width; x++) {
+            t = Infinity;
+            // topLeft + x*width/canvas.width * u - y*height/canvas.height * v
+            // The 0.5 is to start in the middle of the pixel
+            currRay.end = add(topLeft,
+                add(scale((x + 0.5) * width / canvas.width, u),
+                    scale(-(y - 0.5) * height / canvas.height, v)));
 
-	ctx.putImageData(canvasData, 0, 0);
+            config.objects.forEach(intersect);
+
+            // TODO: lighting
+            data.setColor(x, y, (t === Infinity) ? config.background : object.color);
+            // data.setColor(x, y, (t === Infinity) ?
+			// 			  config.background :
+			// 			  object.lighting(normalize(config.camera.eye),
+			// 							  result.normal,
+			// 							  result.point));
+        }
+    }
+
+    ctx.putImageData(canvasData, 0, 0);
 }
--- raytracer_config.js
+++ raytracer_config.js
@@ -1,14 +1,114 @@
+function Entity (args) {
+	this.position = args.position || vec3();
+	this.rotation = args.rotation || vec3();
+	this.scale = args.scale || vec3();
+	this.color = args.color || new Color();
+	this.ambient_constant = vec4(0.2, 0.2, 0.2, 1.0);
+	this.diffuse_constant = vec4(1.0, 0.8, 0.0, 1.0);
+}
+Entity.prototype.lighting = function(eye, normal, point) {
+	var totalColor = vec4(0, 0, 0, 0);
+	var ambient = mult(this.ambient_constant, this.color.toVec4());
+	ambient.forEach(function(value, index) {this[index] = Math.floor(value);}, ambient);
+	totalColor = add(totalColor, ambient);
+	config.lights.forEach(function(light) {
+		// var lightDirection = normalize(subtract(point, light));
+		// reflection = normalize(2 * (lightDirection . normal) * normal - lightDirection)
+		// var reflection = normalize(subtract(mult(scale(2, dot(lightDirection, normal)), normal), lightDirection));
+		// TODO: fix diffuse lighting
+		// var colorScale = scale(dot(lightDirection, normal), this.diffuse_constant);
+		// var colorVec = this.color.toVec4();
+		// var diffuse = mult(colorScale, colorVec);
+		// diffuse.forEach(function(value) {Math.floor(value);});
+		// totalColor = add(totalColor, diffuse);
+		// var specular = mult(scale(ks, pow(dot(reflection, eye)), shininess), ims);
+		// specular.forEach(function(value) {Math.floor(value);});
+		// totalColor = add(totalColor, specular);
+	}, this);
+	return Color.fromVec4(totalColor);
+};
+
+function Sphere (args) {
+	Entity.call(this, {
+		position: args.center,
+		scale: vec3(args.radius, args.radius, args.radius),
+		color: args.color
+	});
+	this.radius = args.radius;
+}
+Sphere.prototype = Object.create(Entity.prototype);
+Sphere.prototype.intersect = function(ray) {
+	var distance = subtract(ray.end, ray.start);
+	var toObject = subtract(ray.start, this.position);
+	var a = dot(distance, distance);
+	var b = dot(scale(2, distance), toObject);
+	var c = dot(toObject, toObject) - this.radius * this.radius;
+	var discriminant = Math.sqrt(b*b - 4*a*c);
+
+	if (discriminant > 0) {
+		var t = (-b - discriminant)/(2*a);
+		var point = add(ray.start, scale(t, distance));
+		return {
+			t: t,
+			point: point,
+			normal: normalize(subtract(point, this.position))
+		};
+	} else {
+		return false;
+	}
+};
+
+function Color (r, g, b, a) {
+	this.r = r || 0;
+	this.g = g || 0;
+	this.b = b || 0;
+	this.a = a || 255;
+}
+Color.prototype.toVec4 = function() {
+	return vec4(this.r, this.g, this.b, this.a);
+};
+Color.fromVec4 = function(vec) {
+	return new Color(vec[0], vec[1], vec[2], vec[3]);
+};
+
+Color.RED = new Color(255, 0, 0);
+Color.YELLOW = new Color(255, 255, 0);
+Color.GREEN = new Color(0, 255, 0);
+Color.CYAN = new Color(0, 255, 255);
+Color.BLUE = new Color(0, 0, 255);
+Color.MAGENTA = new Color(255, 0, 255);
+Color.BLACK = new Color(0, 0, 0);
+Color.WHITE = new Color(255, 255, 255);
+
 var config = {
-	camera: {
-		eye: vec3(5.0, 5.0, 5.0),
-		at: vec3(0.0, 0.0, 0.0),
-		up: vec3(0.0, 1.0, 0.0)
+	camera: { // Camera
+		eye: vec3(5.0, 5.0, 5.0), // Location of the viewer (vec3)
+		at: vec3(0.0, 0.0, 0.0), // Point the viewer is looking at (vec3)
+		up: vec3(0.0, 1.0, 0.0) // "Up" direction relative to eye (vec3)
 	},
-	plane: {
-		distance: 1,
-		fov: 45,
-		aspect: 1
+
+	plane: { // Viewing plane
+		distance: 1, // Distance to viewing plane (int)
+		fov: 45, // Field of view, in degrees (int)
+		aspect: 1 // Aspect Ratio, change if canvas width/height ratio changes (int)
 	},
-	objects: [
+
+	background: Color.BLACK, // Background color
+
+	objects: [ // Objects in the scene
+		new Sphere({
+			center: vec3(-3, 1, -2),
+			radius: 1,
+			color: new Color(0, 85, 170, 255)
+		}),
+		new Sphere({
+			center: vec3(3, -1, -2),
+			radius: 1,
+			color: Color.WHITE
+		})
+	],
+
+	lights: [ // Lights in the scene
+		vec3(5.0, 5.0, 5.0)
 	]
 };
