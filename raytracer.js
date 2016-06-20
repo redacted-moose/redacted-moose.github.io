1a15667c2a29dde974dc04b50d24de7b3c237e3e
Chris Sadler <sadlercr@rose-hulman.edu>
2016-06-20 17:52:56 +0200

    REFLECTIONS WORKgit add raytracer*git add raytracer*!

--- raytracer.js
+++ raytracer.js
@@ -24,9 +24,15 @@ Math.clamp = function(num, min, max) {
 };
 
 function Ray(start, end) {
-    this.start = start || vec3();
-    this.end = end || vec3();
+	this.start = start || vec3();
+	this.end = end || vec3();
 }
+Ray.prototype.distance = function() {
+	return subtract(this.end, this.start);
+};
+Ray.prototype.direction = function() {
+	return normalize(this.distance());
+};
 
 Color.prototype.toVec4 = function() {
 	return vec4(this.r, this.g, this.b, this.a);
@@ -47,8 +53,9 @@ Intersection.prototype.reset = function() {
 };
 Intersection.prototype.lighting = function(camera) {
 	var entity = this.entity;
-	var entityColor = entity.color.toVec4();
-	var ambientColor = mult(entity.ambient_constant, entityColor);
+	// var entityColor = entity.color.toVec4();
+	var entityColor = this.color().toVec4();
+	var ambientColor = mult(entity.constants.ambient, entityColor);
 	var diffuseColor = vec4(0, 0, 0, 0);
 	var specularColor = vec4(0, 0, 0, 0);
 
@@ -65,11 +72,11 @@ Intersection.prototype.lighting = function(camera) {
 
 		// Shadowing
 		// for (var j = 0; j < config.entities.length; j++) {
-		// 	var entity = config.entities[j];
-		// 	var result = entity.intersect(toLightRay);
-		// 	if (result.t < intersection.t) {
-		// 		intersection = result;
-		// 	}
+		//	var entity = config.entities[j];
+		//	var result = entity.intersect(toLightRay);
+		//	if (result.t < intersection.t) {
+		//		intersection = result;
+		//	}
 		// }
 
 		// if (intersection.entity !== entity) continue;
@@ -93,96 +100,109 @@ Intersection.prototype.lighting = function(camera) {
 		var specularFactor = dot(reflection, normalizedCameraVector);
 		specularFactor = Math.max(specularFactor, 0.0);
 
-		var dampedFactor = Math.pow(specularFactor, 50.0);
+		var dampedFactor = Math.pow(specularFactor, entity.shininess);
 
-		var colorScale = scale(brightness / attenuationFactor, entity.diffuse_constant);
+		var colorScale = scale(brightness / attenuationFactor, entity.constants.diffuse);
 		var diffuse = mult(colorScale, lightColor);
 
 		diffuseColor = add(diffuseColor, diffuse);
 
-		// var specular = mult(scale(dampedFactor / attenuationFactor, entity.specular_constant), lightColor);
-		var specular = scale(dampedFactor / attenuationFactor, entity.specular_constant);
+		var specular = scale(dampedFactor / attenuationFactor, entity.constants.specular);
 		specularColor = add(specularColor, specular);
-		specularColor[3] = Math.clamp(specularColor[3], 0.0, 1.0);
 	}
 
 	var totalColor = add(add(ambientColor, mult(diffuseColor, entityColor)), specularColor);
-	// var totalColor = add(ambientColor, mult(diffuseColor, entityColor));
-	// var totalColor = ambientColor;
-	// var totalColor = mult(diffuseColor, entity.color.toVec4());
-	// var totalColor = specularColor;
-	// totalColor = scale(1/totalColor[3], totalColor);
+	// Clamp alpha to 0.0..1.0
 	totalColor[3] = Math.clamp(totalColor[3], 0.0, 1.0);
 
 	return Color.fromVec4(totalColor);
 };
+Intersection.prototype.color = function() {
+	if (this.entity.reflectivity === 0.0) return this.entity.color;
+
+	// console.log(subtract(this.reflection.end, this.reflection.start));
+
+	var intersection = new Intersection();
+	for (var i = 0; i < config.entities.length; i++) {
+		var entity = config.entities[i];
+		var result = entity.intersect(this.reflection);
+		if (0 < result.t && result.t < intersection.t) {
+			intersection = result;
+		}
+	}
+
+	if (this.entity.reflectivity === 1.0) {
+		return (intersection.t < Infinity) ? intersection.lighting(config.camera.eye) : config.background;
+	}
+
+	if (intersection.t < Infinity) {
+		return Color.fromVec4(mix(intersection.entity.color.toVec4(),
+								  this.entity.color.toVec4(),
+								  this.entity.reflectivity));
+	} else {
+		return Color.fromVec4(mix(config.background.toVec4(),
+								  this.entity.color.toVec4(),
+								  this.entity.reflectivity));
+	}
+};
 
 window.onload = function init() {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d');
-    canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    n = normalize(subtract(config.camera.eye, config.camera.at));
-    u = normalize(cross(config.camera.up, n));
-    v = cross(n, u);
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext('2d');
+	canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	n = normalize(subtract(config.camera.eye, config.camera.at));
+	u = normalize(cross(config.camera.up, n));
+	v = cross(n, u);
 
-    height = 2 * config.plane.distance * Math.tan(radians(config.plane.fov) / 2);
-    width = height * config.plane.aspect;
-    // eye - distance*n
-    center = subtract(config.camera.eye, scale(config.plane.distance, n));
+	var aspect = canvas.width / canvas.height;
+
+	height = 2 * config.plane.distance * Math.tan(radians(config.plane.fov) / 2);
+	width = height * aspect;
+	// eye - distance*n
+	center = subtract(config.camera.eye, scale(config.plane.distance, n));
 	// console.log(center);
-    // center - width/2 * u + height/2 * v
-    topLeft = add(subtract(center, scale(width / 2, u)), scale(height / 2, v));
-    render();
+	// center - width/2 * u + height/2 * v
+	topLeft = add(subtract(center, scale(width / 2, u)), scale(height / 2, v));
+	render();
 };
 
 function render() {
 	var intersection = new Intersection();
-    var data = canvasData.data;
-    data.setColor = function(x, y, color) {
-        var index = 4 * (canvas.width * y + x);
-        this[index] = Math.floor(color.r * 255);
-        this[index + 1] = Math.floor(color.g * 255);
-        this[index + 2] = Math.floor(color.b * 255);
-        this[index + 3] = Math.floor(color.a * 255);
-    };
+	var data = canvasData.data;
+	data.setColor = function(x, y, color) {
+		var index = 4 * (canvas.width * y + x);
+		this[index] = Math.floor(color.r * 255);
+		this[index + 1] = Math.floor(color.g * 255);
+		this[index + 2] = Math.floor(color.b * 255);
+		this[index + 3] = Math.floor(color.a * 255);
+	};
 
-    var currRay = new Ray(config.camera.eye);
+	var ray = new Ray(config.camera.eye);
 
-    for (var y = 0; y < canvas.height; y++) {
+	for (var y = 0; y < canvas.height; y++) {
 		var plane_y = scale(-(y - 0.5) * height / canvas.height, v);
-        for (var x = 0; x < canvas.width; x++) {
+		for (var x = 0; x < canvas.width; x++) {
 			var plane_x = scale((x + 0.5) * width / canvas.width, u);
-            intersection.reset();
-            // topLeft + x*width/canvas.width * u - y*height/canvas.height * v
-            // The 0.5 is to start in the middle of the pixel
-            currRay.end = add(topLeft,
-							  add(plane_x, plane_y));
+			intersection.reset();
+			// topLeft + x*width/canvas.width * u - y*height/canvas.height * v
+			// The 0.5 is to start in the middle of the pixel
+			ray.end = add(topLeft,
+						  add(plane_x, plane_y));
 
-            // config.objects.forEach(intersect);
+			// config.objects.forEach(intersect);
 			for (var i = 0; i < config.entities.length; i++) {
 				var entity = config.entities[i];
-				var thing = entity.intersect(currRay);
-				if (thing.t < intersection.t) {
-					intersection = thing;
+				var result = entity.intersect(ray);
+				if (0 < result.t && result.t < intersection.t) {
+					intersection = result;
 				}
 			}
 
-            // TODO: lighting
-			// console.log(t);
-			var color;
-			if (intersection.t < Infinity) {
-				color = intersection.lighting(config.camera.eye);
-			} else {
-				color = config.background;
-			}
-			// if (intersection.t < Infinity) {
-			// 	// console.log(intersection);
-			// 	console.log(color);
-			// }
-            // data.setColor(x, y, (intersection.t === Infinity) ? config.background : intersection.entity.color);
-            data.setColor(x, y, color);
-        }
-    }
+			data.setColor(x, y, (intersection.t < Infinity) ?
+						  intersection.lighting(config.camera.eye) :
+						  config.background);
+		}
+	}
 
-    ctx.putImageData(canvasData, 0, 0);
+	ctx.putImageData(canvasData, 0, 0);
 }
--- raytracer_config.js
+++ raytracer_config.js
@@ -80,10 +80,11 @@ function Light(position, color) {
 	this.attenuation = vec3(1, 0.1, 0.02);
 }
 
-function Intersection(t, point, normal, entity) {
+function Intersection(t, point, normal, reflection, entity) {
 	this.t = t || Infinity;
 	this.point = point || undefined;
 	this.normal = normal || undefined;
+	this.reflection = reflection || undefined;
 	this.entity = entity || undefined;
 }
 
@@ -92,9 +93,13 @@ function Entity(args) {
 	this.rotation = args.rotation || vec3();
 	this.scale = args.scale || vec3();
 	this.color = args.color || new Color();
-	this.ambient_constant = vec4(0.1, 0.1, 0.1, 1.0);
-	this.diffuse_constant = vec4(1.0, 1.0, 1.0, 1.0);
-	this.specular_constant = vec4(1.0, 1.0, 1.0, 1.0);
+	this.reflectivity = args.reflectivity || 0.0;
+	this.constants = args.constants || {
+		ambient: vec4(0.1, 0.1, 0.1, 1.0),
+		diffuse: vec4(1.0, 1.0, 1.0, 1.0),
+		specular: vec4(1.0, 1.0, 1.0, 1.0)
+	};
+	this.shininess = args.shininess || 50.0;
 }
 
 function Sphere(center, radius, color) {
@@ -103,17 +108,21 @@ function Sphere(center, radius, color) {
 		center = args.center;
 		radius = args.radius;
 		color = args.color;
+		reflectivity = args.reflectivity;
 	}
 	Entity.call(this, {
 		position: center,
 		scale: vec3(radius, radius, radius),
-		color: color
+		color: color,
+		reflectivity: reflectivity
 	});
 	this.radius = radius;
 }
 Sphere.prototype = Object.create(Entity.prototype);
 Sphere.prototype.intersect = function(ray) {
 	var distance = subtract(ray.end, ray.start);
+	var reverseDistance = subtract(ray.start, ray.end);
+	var direction = normalize(distance);
 	var toObject = subtract(ray.start, this.position);
 	var a = dot(distance, distance);
 	var b = dot(scale(2, distance), toObject);
@@ -123,10 +132,81 @@ Sphere.prototype.intersect = function(ray) {
 	if (discriminant > 0) {
 		var t = (-b - Math.sqrt(discriminant)) / (2 * a);
 		var point = add(ray.start, scale(t, distance));
-		return new Intersection(t, point, normalize(subtract(point, this.position)), this);
+		var normal = normalize(subtract(point, this.position));
+		// var reflectedVector = normalize(Light.reflect(normal, normalize(negate(point))));
+		var reflectedVector = normalize(Light.reflect(normal, reverseDistance));
+		var reflection = new Ray(point, add(point, reflectedVector));
+		return new Intersection(t, point, normal, reflection, this);
 	} else {
 		return new Intersection();
 	}
+};
+
+function Plane(position, normal, height, width, color, reflectivity) {
+	Entity.call(this, {
+		position: position || vec3(0, 0, 0),
+		color: color || Color.WHITE,
+		reflectivity: reflectivity || 0.0
+	});
+	this.normal = normal || vec3(0.0, 0.0, 1.0);
+	this.width = width || 1.0;
+	this.height = height || 1.0;
+}
+Plane.prototype = Object.create(Entity.prototype);
+Plane.prototype.intersect = function(ray) {
+	var distance = subtract(ray.end, ray.start);
+	var reverseDistance = subtract(ray.start, ray.end);
+	var direction = normalize(distance);
+	var eyeToPlane = subtract(this.position, ray.start);
+	var t = dot(this.normal, eyeToPlane)/dot(this.normal, direction);
+
+	if (t <= 0) return new Intersection();
+
+	var point = add(ray.start, scale(t, distance));
+
+	if (point[0] > (this.position[0] + this.height/2) || point[0] < (this.position[0] - this.height/2)) {
+		return new Intersection();
+	}
+	if (point[1] > (this.position[1] + this.width/2) || point[1] < (this.position[1] - this.width/2)) {
+		return new Intersection();
+	}
+
+	var reflectionVector = normalize(Light.reflect(this.normal, reverseDistance));
+	var reflection = new Ray(point, add(point, reflectionVector));
+	return new Intersection(t, point, this.normal, reflection, this);
+};
+
+function Box(position, height, width, length, color, reflectivity) {
+	Entity.call(this, {
+		position: position || vec3(0, 0, 0),
+		color: color || Color.WHITE,
+		reflectivity: reflectivity || 0.0
+	});
+
+	this.height = height;
+	this.width = width;
+	this.length = length;
+	this.faces = [
+		new Plane(add(position, vec3(-width/2, 0, 0)), vec3(-1, 0, 0), height, length, color, reflectivity),
+		new Plane(add(position, vec3(width/2, 0, 0)), vec3(1, 0, 0), height, length, color, reflectivity),
+		new Plane(add(position, vec3(0,-height/2, 0)), vec3(0, -1, 0), length, width, color, reflectivity),
+		new Plane(add(position, vec3(0,height/2, 0)), vec3(0, 1, 0), length, width, color, reflectivity),
+		new Plane(add(position, vec3(0, 0, -length/2)), vec3(0, 0, -1), height, width, color, reflectivity),
+		new Plane(add(position, vec3(0, 0, length/2)), vec3(0, 0, 1), height, width, color, reflectivity),
+	];
+}
+Box.prototype = Object.create(Entity.prototype);
+Box.prototype.intersect = function(ray) {
+	var intersection = new Intersection();
+	for (var i = 0; i < this.faces.length; i++) {
+		var face = this.faces[i];
+		var result = face.intersect(ray);
+		if (result.t > 0 && result.t < intersection.t) {
+			intersection = result;
+		}
+	}
+
+	return intersection;
 };
 
 // To make new entity types, subclass entity and implement an intersect function, like so:
@@ -141,15 +221,16 @@ Sphere.prototype.intersect = function(ray) {
 
 var config = {
 	camera: { // Camera
-		eye: vec3(0.0, 5.0, 5.0), // Location of the viewer (vec3)
+		eye: vec3(5.0, 5.0, 5.0), // Location of the viewer (vec3)
 		at: vec3(0.0, 0.0, 0.0), // Point the viewer is looking at (vec3)
 		up: vec3(0.0, 1.0, 0.0) // "Up" direction relative to eye (vec3)
 	},
 
 	plane: { // Viewing plane
 		distance: 1, // Distance to viewing plane (float)
-		fov: 45, // Field of view, in degrees (float)
-		aspect: 1 // Aspect Ratio, change if canvas width/height ratio changes (float)
+		fov: 45 // Field of view, in degrees (float)
+		// aspect: 1 // Aspect Ratio, change if canvas width/height ratio changes (float)
+		// Aspect ratio is calculated dynamically from the size of the canvas
 	},
 
 	background: Color.BLACK, // Background color
@@ -163,12 +244,15 @@ var config = {
 		new Sphere({
 			center: vec3(1.5, 0, 0),
 			radius: 1,
-			color: Color.WHITE
-		})
+			color: Color.WHITE,
+			reflectivity: 0.0
+		}),
+		new Plane(vec3(0, 0, -5), vec3(0, 0, 1), 10, 20, Color.WHITE, 1.0),
+		// new Box(vec3(0, 0, 0), 1, 1, 1, Color.GREEN, 0.0)
 	],
 
 	lights: [ // Lights in the scene
-		new Light(vec3(4.5, 0.0, 0.0), Color.GREEN),
-		new Light(vec3(-4.5, 0.0, 0.0), Color.WHITE)
+		new Light(vec3(4.5, 0.0, 5.0), Color.GREEN),
+		new Light(vec3(0.0, 0.0, -2.0), Color.WHITE)
 	]
 };
